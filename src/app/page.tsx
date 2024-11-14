897ea1683f70f477f4d77b4111602f3a5a318ee4
Rafael Almeida <almeidarp.dev@gmail.com>
2024-11-14 15:16:39 -0300

    feat: create WixContext and Provider and also create wixClient connection SDK

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,27 @@
+// "use client";
+
 import CategoryList from "@/components/CategoryList";
 import ProductList from "@/components/ProductList";
 import Slider from "@/components/Slider";
+import {WixClientContext} from "@/context/wixContext";
+import {useWixClient} from "@/hooks/useWixClient";
+import {wixClientServer} from "@/lib/wixClientserver";
+import {useContext, useEffect} from "react";
 
-const HomePage = () => {
+const HomePage = async () => {
+  // const wixClient = useWixClient()
+
+  // useEffect(() => {
+  //   const getProducts = async () => {
+  //     const res = await wixClient.products.queryProducts().find();
+  //     console.log(res);
+  //   };
+  //   getProducts();
+  // }, [wixClient]);
+
+  const wixClient = await wixClientServer();
+  const res = await wixClient.products.queryProducts().find();
+  console.log(res);
   return (
     <div className="">
       <Slider />
