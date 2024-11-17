9532f73a3653c0b6fb9a063fae5e8c026ad65498
Rafael Almeida <almeidarp.dev@gmail.com>
2024-11-16 22:31:52 -0300

    refactor: add categories and informations fetching wix sdk

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -1,156 +1,35 @@
-"use client";
-
+import {wixClientServer} from "@/lib/wixClientserver";
 import Image from "next/image";
 import Link from "next/link";
-import {useEffect, useRef} from "react";
 
-const CategoryList = () => {
-  const scrollContainerRef = useRef<HTMLDivElement | null>(null);
+const CategoryList = async () => {
+  const wixClient = await wixClientServer();
 
-  useEffect(() => {
-    const handleWheelScroll = (event: WheelEvent) => {
-      event.preventDefault();
-      if (scrollContainerRef.current) {
-        scrollContainerRef.current.scrollLeft += event.deltaY;
-      }
-    };
+  const cats = await wixClient.collections.queryCollections().find();
 
-    const container = scrollContainerRef.current;
-    if (container) {
-      container.addEventListener("wheel", handleWheelScroll, {passive: false});
-    }
-
-    return () => {
-      if (container) {
-        container.removeEventListener("wheel", handleWheelScroll);
-      }
-    };
-  }, []);
   return (
-    <div
-      ref={scrollContainerRef}
-      className="px-4 overflow-x-scroll scrollbar-hide transition-all ease-in"
-    >
+    <div className="px-4 overflow-x-scroll scrollbar-hide">
       <div className="flex gap-4 md:gap-8">
-        <Link
-          href="/list?cat=test"
-          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
-        >
-          <div className="relative bg-slate-50-100 w-full h-96">
-            <Image
-              src="/project-wireframe.png"
-              alt=""
-              fill
-              sizes="20vw"
-              className="object-cover"
-            />
-          </div>
-          <h1 className="mt-8 font-light text-cl tracking-wide">
-            Category Name
-          </h1>
-        </Link>
-        <Link
-          href="/list?cat=test"
-          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
-        >
-          <div className="relative bg-slate-50-100 w-full h-96">
-            <Image
-              src="/project-wireframe.png"
-              alt=""
-              fill
-              sizes="20vw"
-              className="object-cover"
-            />
-          </div>
-          <h1 className="mt-8 font-light text-cl tracking-wide">
-            Category Name
-          </h1>
-        </Link>
-        <Link
-          href="/list?cat=test"
-          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
-        >
-          <div className="relative bg-slate-50-100 w-full h-96">
-            <Image
-              src="/project-wireframe.png"
-              alt=""
-              fill
-              sizes="20vw"
-              className="object-cover"
-            />
-          </div>
-          <h1 className="mt-8 font-light text-cl tracking-wide">
-            Category Name
-          </h1>
-        </Link>
-        <Link
-          href="/list?cat=test"
-          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
-        >
-          <div className="relative bg-slate-50-100 w-full h-96">
-            <Image
-              src="/project-wireframe.png"
-              alt=""
-              fill
-              sizes="20vw"
-              className="object-cover"
-            />
-          </div>
-          <h1 className="mt-8 font-light text-cl tracking-wide">
-            Category Name
-          </h1>
-        </Link>
-        <Link
-          href="/list?cat=test"
-          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
-        >
-          <div className="relative bg-slate-50-100 w-full h-96">
-            <Image
-              src="/project-wireframe.png"
-              alt=""
-              fill
-              sizes="20vw"
-              className="object-cover"
-            />
-          </div>
-          <h1 className="mt-8 font-light text-cl tracking-wide">
-            Category Name
-          </h1>
-        </Link>
-        <Link
-          href="/list?cat=test"
-          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
-        >
-          <div className="relative bg-slate-50-100 w-full h-96">
-            <Image
-              src="/project-wireframe.png"
-              alt=""
-              fill
-              sizes="20vw"
-              className="object-cover"
-            />
-          </div>
-          <h1 className="mt-8 font-light text-cl tracking-wide">
-            Category Name
-          </h1>
-        </Link>
-        <Link
-          href="/list?cat=test"
-          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
-        >
-          <div className="relative bg-slate-50-100 w-full h-96">
-            <Image
-              src="/project-wireframe.png"
-              alt=""
-              fill
-              sizes="20vw"
-              className="object-cover"
-            />
-          </div>
-          <h1 className="mt-8 font-light text-cl tracking-wide">
-            Category Name
-          </h1>
-        </Link>
+        {cats.items.map((item) => (
+          <Link
+            href={`/list?cat=${item.slug}`}
+            className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+            key={item._id}
+          >
+            <div className="relative bg-slate-100 w-full h-96">
+              <Image
+                src={item.media?.mainMedia?.image?.url || "cat.png"}
+                alt=""
+                fill
+                sizes="20vw"
+                className="object-cover"
+              />
+            </div>
+            <h1 className="mt-8 font-light text-xl tracking-wide">
+              {item.name}
+            </h1>
+          </Link>
+        ))}
       </div>
     </div>
   );
