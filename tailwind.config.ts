69d9c269235d9c58c6ec27fdf2a6cbb8de34409d
Rafael Almeida <almeidarp.dev@gmail.com>
2024-11-23 23:20:18 -0300

    feat: add react-fast-marquee and styles in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,10 +13,17 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
       colors: {
         rosa: "#F35C7A",
+        "gradient-start": "#3de85d",
+        "gradient-middle": "#03f6f4",
+        "gradient-end": "#937fff",
       },
       screens: {
+        xm: "480px",
         xs: "375px",
         xxs: "320px",
       },
